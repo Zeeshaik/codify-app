b31c56ee5fe47fc3e4aa83b7f1a7983de6d8de9b
Shaik Zeeshan <zeeshaik1431@gmail.com>
2023-10-29 19:39:02 +0530

    Fixed Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,7 +7,7 @@ const Footer: React.FunctionComponent<IFooterProps> = (props) => {
   return <footer className=' bg-dark-layer-1 text-orange-400'>
     <div className=' md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#ffffff19] py-7 md'>
         <h1 className='lg:text-4xl text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold mf:w-2/5'>
-            <span className=' text-teal-50 '>Free</span> until you're ready to <br/>
+            <span className=' text-teal-50 '>Free</span> until you&apos;re ready to <br/>
             lunch
         </h1>
         <div>
